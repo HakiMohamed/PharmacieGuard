40998a32fd46810c3c8ee59b44924eae71df78f7
Mohamed Haki <Mohamedhaki70@gmail.com>
2025-02-06 17:41:20 +0100

    fix: update Dockerfile to handle build args and env variables

---
+++ dist/interfaces/review.interface.d.ts
@@ -0,0 +1,11 @@
+import { FieldValue } from 'firebase-admin/firestore';
+export interface ReviewInterface {
+    id?: string;
+    userId: string;
+    userName: string;
+    pharmacyId: string;
+    rating: number;
+    createdAt: FieldValue;
+    updatedAt: FieldValue;
+    comment?: string;
+}
