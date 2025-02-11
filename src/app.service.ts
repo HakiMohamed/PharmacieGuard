77de8288777e811350f51e1341a7da6cc66978d9
Mohamed Haki <Mohamedhaki70@gmail.com>
2025-02-11 15:07:15 +0100

    test pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Mernops! pipline is working';
+    return 'Hello Mernops! pipline is working iu';
     
   }
 }
